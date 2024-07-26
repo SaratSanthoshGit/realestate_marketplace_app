42defc4121acfce563d5aa74848a823184bec16f
saratsanthosh96 <saratsanthosh96@gmail.com>
2024-07-26 04:51:31 -0400

    Remove unused files and restructure project directory part 3, setup for rest of code to be plugged in.

--- lib/Utils/navigation_service.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-
-class NavigationService {
-  static void navigateTo(BuildContext context, Widget screen) {
-    Navigator.push(context, MaterialPageRoute(builder: (_) => screen));
-  }
-}
